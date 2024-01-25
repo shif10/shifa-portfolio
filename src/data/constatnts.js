4eb20dc1d6a3b7e3d12edda87642367b104b6324
shifa <shifabhari@gmail.com>
2024-01-25 16:48:13 +0530

    chnage in names

--- src/data/constatnts.js
+++ src/data/constatnts.js
@@ -1,5 +1,5 @@
 export const Bio = {
-  name: "shifa bhari",
+  name: "Shifa Bhari",
   roles: ["Full Stack Developer", "MERN Stack Developer", "Programmer"],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
