74442d46f0c287160bf1d9557dec49ca9ab4a37c
Shifa <shifabhari@gmail.com>
2026-09-18 17:29:13 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { ThemeProvider } from "styled-components";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { darkTheme, lightTheme } from "./utils/Themes.js";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 // import { HeroSection } from "./components/HeroSection/Hero.jsx";
-import About from "./components/About";
 import { Navbar } from "./components/navbar/Navbar.jsx";
 import { Hero } from "./components/herosection/Hero.jsx";
 import Projects from "./components/Projects/index.js";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Experience from "./components/Experience";
 import ProjectDetails from "./components/ProjectDetails";
@@ -38,7 +36,7 @@ const Wrapper = styled.div`
   clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
 `;
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode] = useState(false);
   const [openModal, setOpenModal] = useState({ state: false, project: null });
   console.log(openModal);
   return (
